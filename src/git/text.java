d828f2246b24e368bc01f990cf2c698c31fe4a1f
wxz <932094697@qq.com>
2022-09-01 20:57:24 +0800

    hot-fix commit 1

--- src/git/text.java
+++ src/git/text.java
@@ -2,6 +2,7 @@ package git;
 
 public class text {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
